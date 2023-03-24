46385603120a4ac2a715d45ee890553ee952741f
Nika Chapidze <nika.chapidze@flatrocktech.com>
2023-03-24 09:50:47 +0400

    add firebase release notes file

--- app/src/main/java/com/example/composerecorder/MainActivity.kt
+++ app/src/main/java/com/example/composerecorder/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+//            test change
             AppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
