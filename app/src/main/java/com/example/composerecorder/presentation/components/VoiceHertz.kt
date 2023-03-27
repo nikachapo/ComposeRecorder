ba74b3fd52b17ec266b4dcefb3e12d4176f4bf83
Nika Chapidze <nika.chapidze@flatrocktech.com>
2023-03-27 18:06:06 +0400

    fix remember

--- app/src/main/java/com/example/composerecorder/presentation/components/VoiceHertz.kt
+++ app/src/main/java/com/example/composerecorder/presentation/components/VoiceHertz.kt
@@ -1,14 +1,17 @@
 package com.example.composerecorder.presentation.components
 
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -19,9 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
 fun VoiceHertz(
@@ -35,7 +36,7 @@ fun VoiceHertz(
     isPlaying: Boolean,
 ) {
 
-    val animatedValues = rememberSaveable(wavesCount) {
+    val animatedValues = remember(wavesCount) {
         mutableStateListOf<Animatable<Float, AnimationVector1D>>().apply {
             (1..wavesCount).map { add(Animatable(0f)) }
         }
